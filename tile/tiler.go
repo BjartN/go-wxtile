14547b36f67f967c0826cd87b0d3b45dbfd66de1
Bjarte Djuvik Næss <bjartn@gmail.com>
2015-12-16 21:15:12 +0100

    Example on creating tiles based on grib2json file

--- tile/tiler.go
+++ tile/tiler.go
@@ -16,25 +16,22 @@ func Tiler(g Grid, maxZoom int, targetFolder string){
 		Point{MercatorPole*-1,MercatorPole*-1},
 		Point{MercatorPole,MercatorPole}}
 
-	totalNumTiles:=4*(math.Pow(4,float64(maxZoom))-1)/3
-
 	var wg sync.WaitGroup
-	wg.Add(int(totalNumTiles))
-
-	fmt.Println(totalNumTiles)
 
 	for z:=1; z<=maxZoom; z++ {
 		sizef := math.Pow(2,float64(z))
 		for x:=0; x<int(sizef); x++ {
 			xf := float64(x)
 			for y:=0; y<int(sizef); y++ {
+				wg.Add(1)
+
 				width := world.Width() / sizef
 				yf := float64(y)
 
 				//make bounds
 				b := Bounds{
-					Point{width * xf, MercatorPole - width * (yf + 1)},
-					Point{width * (xf + 1), MercatorPole - width * yf}}
+					Point{MercatorPole*-1 +  width * xf, MercatorPole - width * (yf + 1)},
+					Point{MercatorPole*-1 +  width * (xf + 1), MercatorPole - width * yf}}
 
 				fileName := fmt.Sprintf(targetFolder + "\\" + strconv.Itoa(int(z)) + "_" + strconv.Itoa(int(x)) + "_" + strconv.Itoa(int(y)) + ".png")
 
