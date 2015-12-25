072852a19817dc3b04f6e140b79160d999baf7d4
Bjarte Djuvik Næss <bjartn@gmail.com>
2015-12-25 11:01:51 +0100

    Cleaned up some bad test code

--- tile/projection_test.go
+++ tile/projection_test.go
@@ -1,13 +1,12 @@
 package tile
 
 import (
-	"testing"
-	"github.com/bjartn/go-wxtile/tile"
 	"fmt"
+	"testing"
 )
 
-func TestShouldProject(t *testing.T){
-	m := tile.MercatorProjector{}
+func TestShouldProject(t *testing.T) {
+	m := MercatorProjector{}
 
 	/*
 		180.0625 -50.869140625
@@ -16,13 +15,13 @@ func TestShouldProject(t *testing.T){
 		2.003055087182639e+07 -6.59156675680389e+06
 	*/
 
-	lng:=180.0625
-	lat:=-50.869140625
-	p:= m.ToProjection(lng,lat)
+	lng := 180.0625
+	lat := -50.869140625
+	p := m.ToProjection(lng, lat)
 	fmt.Println(p.X, p.Y)
 
-	lng2:=179.9375
-	lat2:=-50.831640625000006
-	p2:= m.ToProjection(lng2,lat2)
+	lng2 := 179.9375
+	lat2 := -50.831640625000006
+	p2 := m.ToProjection(lng2, lat2)
 	fmt.Println(p2.X, p2.Y)
 }
--- tile/telp.go
+++ tile/telp.go
@@ -1,13 +1,13 @@
 package tile
 
-import(
-	"testing"
+import (
 	"math"
+	"testing"
 )
 
 //AssetAreEqual check if two float64 numbers are roughly equal, and fails the test if not.
-func AssertAreEqual(t *testing.T, expected float64, actual float64, message string){
-	if math.Abs(expected-actual)>0.001 {
+func AssertAreEqual(t *testing.T, expected float64, actual float64, message string) {
+	if math.Abs(expected-actual) > 0.001 {
 		t.Log(message)
 		t.Log("Should be ", expected, " was ", actual)
 		t.Fail()
